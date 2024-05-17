39d752ffdfdfe8e19e602c7f368e8e07163be140
Th0masL3 <2230852@champlaincollege.qc.ca>
2024-05-16 23:22:01 -0400

    Put main activity instead of main page

--- app/src/main/java/com/example/dailydrive/MainActivity.kt
+++ app/src/main/java/com/example/dailydrive/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.example.dailydrive
 
-import android.app.Fragment
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -13,15 +13,13 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [MainPage.newInstance] factory method to
+ * Use the [MainActivity.newInstance] factory method to
  * create an instance of this fragment.
  */
-class MainPage : Fragment() {
+class MainActivity : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
-
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -46,20 +44,16 @@ class MainPage : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment MainPage.
+         * @return A new instance of fragment MainActivity.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            MainPage().apply {
+            MainActivity().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
                 }
             }
     }
-
-    //my code
-    val dailyScoreString: String = getString(R.string.dailyScore)
-    val coinAmountString: String = getString(R.string.coinAmount)
 }
